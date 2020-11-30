c67ab34966212ad979b7726fd7e103cab5657586
Natalia Karanevich <natallia.karanevich@itechart-group.com>
2020-11-30 19:36:15 +0300

    Init Project: test commit

--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -1,24 +1,19 @@
 <template>
-          <Container>
-                    <div class="edit-page">
-                              <h1>{{ $route.params.id ? 'Edit Todo' : 'Create Todo' }}</h1>
-                              <form @submit.prevent="updateTodo">
-                                        <label>
-                                                  <Input
-                                                            placeholder="Enter todo"
-                                                            type="text"
-                                                            required
-                                                            v-model="todo.title"
-                                                  />
-                                        </label>
-                                        <Button type="submit" icon="pi pi-check" label="Submit" />
-                              </form>
-                              <div>
-                                        <h2>Dynamic components</h2>
-                                        <dynamic-components />
-                              </div>
-                    </div>
-          </Container>
+  <Container>
+    <div class="edit-page">
+      <h1>{{ $route.params.id ? 'Edit Todo' : 'Create Todo' }}</h1>
+      <form @submit.prevent="updateTodo">
+        <label>
+          <Input placeholder="Enter todo" type="text" required v-model="todo.title" />
+        </label>
+        <Button type="submit" icon="pi pi-check" label="Submit" />
+      </form>
+      <div>
+        <h2>Dynamic components</h2>
+        <dynamic-components />
+      </div>
+    </div>
+  </Container>
 </template>
 
 <script lang="ts">
@@ -26,41 +21,41 @@ import { defineComponent } from 'vue';
 import { Todo } from '@/models/interfaces';
 
 export default defineComponent({
-          name: 'edit',
-          components: {
-                    Container: () => import('@/components/container.vue'),
-                    Input: () => import('primevue/components/inputtext/InputText'),
-                    Button: () => import('primevue/components/button/Button'),
-                    DynamicComponents: () => import('@/components/dynamic-component.vue')
-          },
-          created() {
-                    if (this.$route.params.id) {
-                              this.$store.dispatch('getTodoById', this.$route.params.id);
-                    }
-          },
-          computed: {
-                    todo(): Todo {
-                              const id = this.$route.params.id;
-                              return id ? this.$store.getters.currentTodo : { title: '', done: false };
-                    }
-          },
-          methods: {
-                    updateTodo() {
-                              const id = this.$route.params.id;
-                              this.$store.dispatch(id ? 'updateTodo' : 'createTodo', {
-                                        id: id && id,
-                                        title: this.todo.title,
-                                        done: false
-                              });
-                    }
-          }
+  name: 'edit',
+  components: {
+    Container: () => import('@/components/container.vue'),
+    Input: () => import('primevue/components/inputtext/InputText'),
+    Button: () => import('primevue/components/button/Button'),
+    DynamicComponents: () => import('@/components/dynamic-component.vue')
+  },
+  created() {
+    if (this.$route.params.id) {
+      this.$store.dispatch('getTodoById', this.$route.params.id);
+    }
+  },
+  computed: {
+    todo(): Todo {
+      const id = this.$route.params.id;
+      return id ? this.$store.getters.currentTodo : { title: '', done: false };
+    }
+  },
+  methods: {
+    updateTodo() {
+      const id = this.$route.params.id;
+      this.$store.dispatch(id ? 'updateTodo' : 'createTodo', {
+        id: id && id,
+        title: this.todo.title,
+        done: false
+      });
+    }
+  }
 });
 </script>
 
 <style lang="scss" scoped>
 .edit-page {
-          label {
-                    margin-right: 20px;
-          }
+  label {
+    margin-right: 20px;
+  }
 }
 </style>
