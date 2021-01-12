6dc10f7da1fb06adc700a38cfd2d5bfb65e0a053
Natalia Karanevich <natallia.karanevich@itechart-group.com>
2021-01-12 12:35:14 +0300

    Setup scripts

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,6 +29,12 @@ module.exports = {
       env: {
         jest: true
       }
+    },
+    {
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+      env: {
+        jest: true
+      }
     }
   ]
 };
